52fa600a7d4c2a4ae0632b26b146551f2105873b
Sephin Sebastian E <sephinsebastian0830@gmail.com>
2025-07-22 03:11:40 +0530

    api tested

---
+++ marginalia-next-frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Navbar() {
+    return(
+        <nav className="bg-[#8B4029] text-white px-4 py-3 flex justify-between items-center">
+            <div className="">Marginalia</div>
+        </nav>
+    );
+};
